b79878ef3c4556aa2cc705a2f008f1e6ab394fae
Eman Masoud <114053062+E1442020@users.noreply.github.com>
2023-09-02 17:00:48 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import "./globals.css";
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Navbar } from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
